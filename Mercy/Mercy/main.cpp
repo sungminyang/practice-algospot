9d06ccdabae4b85150890776c9249948e9840519
Yang Sungmin <smy0906@gmail.com>
2017-03-21 18:01:37 +0900

    Mercy

---
+++ Mercy/Mercy/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Mercy
+//
+//  Created by Yang on 2017. 3. 21..
+//  Copyright © 2017년 MyandB. All rights reserved.
+//
+
+#include <iostream>
+
+int main() {
+    int count;
+    std::cin >> count;
+    for (int i = 0; i < count; i++) {
+        std::cout << "Hello Algospot!\n";
+    }
+    return 0;
+}
